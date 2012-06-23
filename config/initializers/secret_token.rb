0bc0c0909056e1b35ee56bef352d4c09a7d7d073
Matthew Burket <matthew@assignitappp.com>
2012-06-23 10:02:46 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iscore::Application.config.secret_token = 'b55df761a98a00dc48b006a41072360650286dbae07100657afa639f695d8d6752e5ae48a1a1b9b83709b200ef506cfc1ec1dec6a7481ab2ac0698d6410da599'
