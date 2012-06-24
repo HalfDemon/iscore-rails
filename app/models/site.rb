21d2a99e261844fee9393ffbf11d034e68ff1578
Matthew Burket <matthew@assignitappp.com>
2012-06-23 21:39:45 -0500

    Added docs [ci skip]

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,6 @@
+# == Blue Teams
+# Sites or Blue Teams are the groups of people in the CDC
+# a user belongs to a site
 class Site < ActiveRecord::Base
   attr_accessible :name, :number, :organization, :points
 end
