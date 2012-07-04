085d58ef80d1d52e5c9b2a6a4e08758f2f0410ff
Matthew Burket <matthew@assignitapp.com>
2012-07-03 20:21:10 -0500

    Worked on the docs

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,19 @@
 # == Blue Teams
 # Sites or Blue Teams are the groups of people in the CDC
 # a user belongs to a site
+
 class Site < ActiveRecord::Base
   attr_accessible :name, :number, :organization, :points
+
+  validates_presence_of :name, :number, :organization
+
+
+  validates_uniqueness_of :number
+
+  
+  before_save :set_default_values
+
+  def set_default_values
+    self.points = 0
+  end
 end
