663b06b9ea81c341c16ae84fad25a527ad6570c4
Matthew Burket <matthew@assignitapp.com>
2012-07-25 20:53:37 -0500

    Work on factories Name the Site Numbers Work Right Along with fix the sample data

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -1,10 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :name do |n|
+  	"Site #{n}"
+  end
+
+  sequence :number do |n|
+  	"#{n}"
+  end
+
   factory :site do
-    name "MyString"
-    organization "MyString"
-    points ""
-    number "MyString"
+  	name
+    organization "Iowa State University"
+    points "0"
+    number
   end
 end
