17edf1e7d209395f43170fae1f326f9936c1a488
Matthew Burket <Matthew@AssignItApp.com>
2012-12-24 13:12:26 -0600

    Added validations for services.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ActiveRecord::Base
   attr_accessible :name, :port, :site_id, :subdomain
+
+  validates_presence_of :name, :port, :site_id, :subdomain
+
+  validates_numericality_of :port, :site_id
 end
