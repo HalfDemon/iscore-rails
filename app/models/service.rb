34b2cebcde6d773d5ae8ccad3a820d0fa760c00e
Matthew Burket <Matthew@AssignItApp.com>
2012-12-27 21:43:07 -0600

    Added Service associations

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,9 @@
 class Service < ActiveRecord::Base
   attr_accessible :name, :port, :site_id, :subdomain
 
-  validates_presence_of :name, :port, :site_id, :subdomain
+  validates_presence_of :name, :port, :site_id, :subdomain, :service_type
 
   validates_numericality_of :port, :site_id
+
+  belongs_to :service_type
 end
