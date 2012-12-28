de3b05deeacc48a10ca2934cf0002137e0e1b1ae
Matthew Burket <Matthew@AssignItApp.com>
2012-12-27 21:45:49 -0600

    Added associations for Usbility reports submissions

--- app/models/usability_reports_submission.rb
+++ app/models/usability_reports_submission.rb
@@ -1,4 +1,6 @@
 class UsabilityReportsSubmission < ActiveRecord::Base
+  belongs_to :usability_reports_check
+
   attr_accessible :comments, :done_by, :site_id
 
   validates_presence_of :site_id, :done_by, :site_id
